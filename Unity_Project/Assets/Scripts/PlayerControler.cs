9466f859a3d716bb1945357e7f0b7115e73d4548
FluffMeister0g <jonathantr@myyahoo.com>
2024-09-10 10:20:21 -0700

    cam/player movement

--- Unity_Project/Assets/Scripts/PlayerControler.cs
+++ Unity_Project/Assets/Scripts/PlayerControler.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     Rigidbody myRB;
+    Camera playercam;
+    Vector2 camRotation;
+
     public float speed = 10f;
+    public float jumpHeight = 5f;
+    //mouse movement
+    public float mouseSensitivity = 2.0f;
+    public float xSensitivity = 2.0f;
+    public float ySensitivity = 2.0f;
+    public float camRotationLimit = 90f;
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
+        playercam = transform.GetChild(0).GetComponent<Camera>();
+        Cursor.visible = false;
+        camRotation = Vector2.zero;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            myRB.AddForce(transform.forward * speed);
-        }
+       // camera movement
+        Quaternion mouseLook = playercam.transform.rotation;
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -90, 90);
+
+        playercam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+        // movement movement
+        Vector3 temp = myRB.velocity;
+        temp.x = Input.GetAxisRaw("Horizontal") * speed;
+        temp.z = Input.GetAxisRaw("Vertical") * speed;
+        if (Input.GetKeyDown(KeyCode.Space))
+            temp.y = jumpHeight;
+        myRB.velocity = (transform.forward * temp.z) + (transform.right * temp.x) + (transform.up * temp.y);
+        
     }
 }
